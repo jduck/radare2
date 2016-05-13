83c7661af6b3f4e5cebd3cf9eac3e84948d0b069
Joshua J. Drake <github.jdrake@qoop.org>
2016-05-13 13:05:06 -0500

    Expose single-breakpoint handling directly

--- libr/bp/io.c
+++ libr/bp/io.c
@@ -3,6 +3,24 @@
 #include <r_bp.h>
 #include "../config.h"
 
+// TODO: rename from r_debug_ ...
+
+R_API void r_bp_restore_one(RBreakpoint *bp, RBreakpointItem *b, int set) {
+	if (set) {
+		eprintf ("Setting bp at 0x%08"PFMT64x"\n", b->addr);
+		if (b->hw || !b->bbytes)
+			eprintf ("hw breakpoints not yet supported\n");
+		else
+			bp->iob.write_at (bp->iob.io, b->addr, b->bbytes, b->size);
+	} else {
+		eprintf ("Clearing bp at 0x%08"PFMT64x"\n", b->addr);
+		if (b->hw || !b->obytes)
+			eprintf ("hw breakpoints not yet supported\n");
+		else
+			bp->iob.write_at (bp->iob.io, b->addr, b->obytes, b->size);
+	}
+}
+
 /**
  * reflect all r_bp stuff in the process using dbg->bp_write or ->breakpoint
  */
@@ -26,25 +44,9 @@ R_API bool r_bp_restore_except(RBreakpoint *bp, int set, ut64 addr) {
 		}
 		if (bp->breakpoint && bp->breakpoint (b, set, bp->user)) {
 			continue;
-		}
-		/* write obytes from every breakpoint in r_bp if not handled by plugin */
-		if (set) {
-			//eprintf ("Setting bp at 0x%08"PFMT64x"\n", b->addr);
-			if (b->hw || !b->bbytes) {
-				eprintf ("hw breakpoints not yet supported\n");
-			} else {
-				bp->iob.write_at (bp->iob.io, b->addr, b->bbytes, b->size);
-				rc = true;
-			}
-		} else {
-			//eprintf ("Clearing bp at 0x%08"PFMT64x"\n", b->addr);
-			if (b->hw || !b->obytes) {
-				eprintf ("hw breakpoints not yet supported\n");
-			} else {
-				bp->iob.write_at (bp->iob.io, b->addr, b->obytes, b->size);
-				rc = true;
-			}
-		}
+
+		/* write (o|b)bytes from every breakpoint in r_bp if not handled by plugin */
+		r_bp_restore_one (bp, b, set);
 	}
 	return rc;
 }
